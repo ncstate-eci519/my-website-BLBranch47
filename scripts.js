c736d626b3e44087662c386b05831b8e7fec3624
BLBranch47 <77651641+BLBranch47@users.noreply.github.com>
2021-04-08 12:53:03 -0400

    API demo

--- scripts.js
+++ scripts.js
@@ -19,3 +19,21 @@ $(document).ready(function(){
       $("h1").text("Hello, "+name+"!");
     });
     });
+
+    $(document).ready(function(){
+        //Run when the button is clicked
+        $("button").click(function(){
+          //Execute the API call.
+          var cat = $.get("https://api.thecatapi.com/v1/images/search");
+          
+          //Function that runs when the API runs successfully
+          cat.done(function(response){
+            //Because the API returns an array, get the first element.
+            var catData = response[0];
+            //Show the cat block
+            $("#cat").show();
+            //Set the SRC of the image to the 
+            $("#cat-img").attr("src", catData.url);
+          });
+        });
+      });
