3a7f23105d0b50cf58e8eb754ad31bbb94adeb07
BLBranch47 <77651641+BLBranch47@users.noreply.github.com>
2021-04-08 19:32:45 -0400

    nav bar fix

--- scripts.js
+++ scripts.js
@@ -22,18 +22,17 @@ $(document).ready(function(){
 
     $(document).ready(function(){
         //Run when the button is clicked
-        $("button").click(function(){
+        $("#cat-button").click(function(){
           //Execute the API call.
-          var cat = $.get("https://api.thecatapi.com/v1/images/search");
+          var cat = $.get("https://dog.ceo/api/breeds/image/random");
           
           //Function that runs when the API runs successfully
           cat.done(function(response){
             //Because the API returns an array, get the first element.
-            var catData = response[0];
             //Show the cat block
             $("#cat").show();
             //Set the SRC of the image to the 
-            $("#cat-img").attr("src", catData.url);
+            $("#cat-img").attr("src", response.message);
           });
         });
       });
