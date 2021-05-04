cfdaf3591f8414a96ec7a3019f144db0b3672800
BLBranch47 <77651641+BLBranch47@users.noreply.github.com>
2021-05-04 19:22:40 -0400

    fixes

--- scripts.js
+++ scripts.js
@@ -21,6 +21,36 @@ $(document).ready(function(){
     });
 
     $(document).ready(function(){
+      $("form").submit(function(){
+        event.preventDefault();
+        var toSubmit = {};
+        toSubmit.email = $("#email").val();
+        toSubmit.name = $("#name").val();
+        toSubmit.message = $("#message").val();
+        $.ajaxSetup({
+          headers: {
+            'Accept': "application/json",
+          }
+        });
+        
+        var form_submit = $.post("https://formspree.io/f/mqkwawqv", toSubmit);
+        
+        form_submit.done(function(){
+          $("#form-success").modal('show');
+          $("#email").val("");
+          $("#name").val("");
+          $("#message").val("");
+        });
+        
+        form_submit.fail(function(){
+          $("#form-error").modal('show');
+        });
+        
+        
+      })
+    });
+
+    $(document).ready(function(){
         //Run when the button is clicked
         $("#cat-button").click(function(){
           //Execute the API call.
